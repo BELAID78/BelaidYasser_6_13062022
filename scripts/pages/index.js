4f8997dfeb23a3b56261a0809f0279d201a4a422
Belaid <yasser.bel93@gmail.com>
2022-06-14 01:40:56 +0200

    refactor the code for index page

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,23 +1,21 @@
-async function getPhotographers() {
+class Index {
+    constructor() {
+        this.$photographerWrapper   = document.querySelector(".photographer_section");
+        this.photographerApi        = new PhotographerApi('/data/photographers.json')
+    }
 
-    const photographers = await new PhotographerApi('/data/photographers.json').getPhotographers();
+    async main() {
+        const photographerData = await this.photographerApi.getPhotographers();
 
-    return photographers
+        const photographers = await photographerData.photographers.map(photographer => new Photographer(photographer));
+
+        photographers.map(photographer => {
+            const photographerCard = new PhotographerCard(photographer).createPhotographerCard()
+            this.$photographerWrapper.appendChild(photographerCard)
+        })
+    }
 }
 
-async function displayData(photographers) {
-    const photographersSection = document.querySelector(".photographer_section");
+const index = new Index()
 
-    photographers.forEach((photographer) => {
-        const photographerModel = photographerFactory(photographer);
-        const userCardDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
-    });
-};
-
-async function init() {
-    const { photographers } = await getPhotographers();
-    displayData(photographers);
-};
-
-init();
+index.main();
